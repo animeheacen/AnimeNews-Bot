b3b5412684c7ab94d171f0b47e581c9084bbdfc8
animeheacen <pavanvallu2@gmail.com>
2025-05-30 08:22:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # config.py
 
-API_ID = "" #API ID get it from telegram.org
-API_HASH = "" #API HASH get it from telegram.org
+API_ID = "20793620" #API ID get it from telegram.org
+API_HASH = "a712d2b8486f26c4dee5127cc9ae0615" #API HASH get it from telegram.org
 BOT_TOKEN = "" #Bot Token get it from @BotFather
 URL_A = "" #News RSS Feed Url 2
 #URL_B = "" #News RSS Feed Url 2
-START_PIC = "" #Startup Pic
-MONGO_URI = "" #Databse uri get it from https://www.mongodb.com/
-ADMINS = "" #Admin ID
+START_PIC = "https://i.ibb.co/Q7rkTBtT/a4305965e9b4.jpg" #Startup Pic
+MONGO_URI = "mongodb+srv://pokemonchannel098:yaE7BvFwWIXdb3HQ@cluster0.gdr57.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" #Databse uri get it from https://www.mongodb.com/
+ADMINS = "6853851676" #Admin ID
