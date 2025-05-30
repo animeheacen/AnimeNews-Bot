034dd9fb933cb77e44b343bd65fc5a51ee409d05
animeheacen <pavanvallu2@gmail.com>
2025-05-30 08:24:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@
 
 API_ID = "20793620" #API ID get it from telegram.org
 API_HASH = "a712d2b8486f26c4dee5127cc9ae0615" #API HASH get it from telegram.org
-BOT_TOKEN = "http://myanimelist.net/rss.php?type=news" #Bot Token get it from @BotFather
-URL_A = "" #News RSS Feed Url 2
-#URL_B = "" #News RSS Feed Url 2
+BOT_TOKEN = "8136710643:AAHrakx2QkLny5z_3NQRVcCs9djE9DwSPOQ" #Bot Token get it from @BotFather
+URL_A = "http://myanimelist.net/rss.php?type=news" #News RSS Feed Url 2
+#URL_B = "https://feeds.feedburner.com/crunchyroll/rss" #News RSS Feed Url 2
 START_PIC = "https://i.ibb.co/Q7rkTBtT/a4305965e9b4.jpg" #Startup Pic
 MONGO_URI = "mongodb+srv://pokemonchannel098:yaE7BvFwWIXdb3HQ@cluster0.gdr57.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" #Databse uri get it from https://www.mongodb.com/
 ADMINS = "6853851676" #Admin ID
